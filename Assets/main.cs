5fce9db24cc1e962f29f8cf6abe603cc2b3db142
Jason Tsorlinis <jtsorlinis@gmail.com>
2022-09-19 02:11:42 +1000

    Added unity job mode

--- Assets/main.cs
+++ Assets/main.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
 
 struct Boid
 {
@@ -15,11 +18,10 @@ struct Boid
 
 public class main : MonoBehaviour
 {
-
-
   [Header("Performance")]
   [SerializeField] int numBoids = 500;
-  [SerializeField] bool useGPU = false;
+  enum Modes { Cpu, Jobs, Gpu };
+  Modes mode = Modes.Cpu;
 
   [Header("Settings")]
   [SerializeField] float maxSpeed = 2;
@@ -34,7 +36,7 @@ public class main : MonoBehaviour
   [SerializeField] GameObject boidPrefab;
   [SerializeField] Text fpsText;
   [SerializeField] Text boidText;
-  [SerializeField] Text modeText;
+  [SerializeField] Dropdown modeSelector;
   [SerializeField] ComputeShader boidShader;
   [SerializeField] Material boidMat;
   [SerializeField] Mesh quad;
@@ -42,23 +44,24 @@ public class main : MonoBehaviour
   float minSpeed;
   float turnSpeed;
 
-  Boid[] boids;
+  NativeArray<Boid> boids;
+  NativeArray<Boid> boids2;
+  BoidBehavioursJob boidJob = new BoidBehavioursJob();
 
   ComputeBuffer boidBuffer;
 
   float xBound, yBound;
   Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 100);
 
-  bool switchingModes = false;
-  float cpuLimit = 3000;
+  int jobLimit = 30000;
+  int cpuLimit = 3000;
 
   // Start is called before the first frame update
   void Start()
   {
-    modeText.text = useGPU || numBoids > cpuLimit ? "Mode: GPU" : "Mode: CPU";
-    switchingModes = useGPU;
     boidText.text = "Boids: " + numBoids;
-    boids = new Boid[numBoids];
+    boids = new NativeArray<Boid>(numBoids, Allocator.Persistent);
+    boids2 = new NativeArray<Boid>(numBoids, Allocator.Persistent);
     xBound = Camera.main.orthographicSize * Camera.main.aspect - edgeMargin;
     yBound = Camera.main.orthographicSize - edgeMargin;
     turnSpeed = maxSpeed * 3;
@@ -99,22 +102,7 @@ public class main : MonoBehaviour
   {
     fpsText.text = "FPS: " + (int)(1 / Time.smoothDeltaTime);
 
-    if (useGPU != switchingModes)
-    {
-      if (useGPU && numBoids <= cpuLimit)
-      {
-        modeText.text = "Mode: GPU";
-        boidBuffer.SetData(boids);
-      }
-      else
-      {
-        modeText.text = "Mode: CPU";
-        boidBuffer.GetData(boids);
-      }
-      switchingModes = useGPU;
-    }
-
-    if (useGPU || numBoids > cpuLimit)
+    if (mode == Modes.Gpu)
     {
       boidShader.SetFloat("deltaTime", Time.deltaTime);
       boidShader.SetFloat("cohesionFactor", cohesionFactor);
@@ -125,10 +113,30 @@ public class main : MonoBehaviour
     }
     else
     {
+      if (mode == Modes.Jobs)
+      {
+        boidJob.inBoids = boids;
+        boidJob.outBoids = boids2;
+        boidJob.deltaTime = Time.deltaTime;
+        boidJob.numBoids = numBoids;
+        boidJob.visualRange = visualRange;
+        boidJob.minDistance = minDistance;
+        boidJob.cohesionFactor = cohesionFactor;
+        boidJob.alignmentFactor = alignmentFactor;
+        boidJob.seperationFactor = seperationFactor;
+
+        JobHandle handle = boidJob.Schedule(numBoids, 8);
+        handle.Complete();
+        boids.CopyFrom(boids2);
+      }
+
       for (int i = 0; i < numBoids; i++)
       {
         var boid = boids[i];
-        MergedBehaviours(ref boid);
+        if (mode == Modes.Cpu)
+        {
+          MergedBehaviours(ref boid);
+        }
         LimitSpeed(ref boid);
         KeepInBounds(ref boid);
 
@@ -142,7 +150,6 @@ public class main : MonoBehaviour
     }
 
     Graphics.DrawMeshInstancedProcedural(quad, 0, boidMat, bounds, numBoids);
-    switchingModes = useGPU;
   }
 
   void MergedBehaviours(ref Boid boid)
@@ -214,9 +221,94 @@ public class main : MonoBehaviour
       boid.vel.y += Time.deltaTime * turnSpeed;
     }
   }
+  [BurstCompile]
+  struct BoidBehavioursJob : IJobParallelFor
+  {
+    [ReadOnly]
+    public NativeArray<Boid> inBoids;
+    public NativeArray<Boid> outBoids;
+    public float deltaTime;
+    public int numBoids;
+    public float visualRange;
+    public float minDistance;
+    public float cohesionFactor;
+    public float alignmentFactor;
+    public float seperationFactor;
+
+    public void Execute(int index)
+    {
+      Boid boid = inBoids[index];
+      Vector2 center = Vector2.zero;
+      Vector2 close = Vector2.zero;
+      Vector2 avgVel = Vector2.zero;
+      int neighbours = 0;
+
+      for (int i = 0; i < numBoids; i++)
+      {
+        var distance = Vector2.Distance(boid.pos, inBoids[i].pos);
+        if (distance < visualRange)
+        {
+          if (distance < minDistance)
+          {
+            close += boid.pos - inBoids[i].pos;
+          }
+          center += inBoids[i].pos;
+          avgVel += inBoids[i].vel;
+          neighbours++;
+        }
+      }
+
+      if (neighbours > 0)
+      {
+        center /= neighbours;
+        avgVel /= neighbours;
+
+        boid.vel += (center - boid.pos) * cohesionFactor * deltaTime;
+        boid.vel += (avgVel - boid.vel) * alignmentFactor * deltaTime;
+      }
+
+      boid.vel += close * seperationFactor * deltaTime;
+      outBoids[index] = boid;
+    }
+  }
+
+  public void modeChange(int val)
+  {
+    // CPU
+    if (val == 0)
+    {
+      mode = Modes.Cpu;
+      var tempArray = new Boid[numBoids];
+      boidBuffer.GetData(tempArray);
+      boids.CopyFrom(tempArray);
+    }
+
+    // CPU Jobs
+    if (val == 1)
+    {
+      mode = Modes.Jobs;
+      var tempArray = new Boid[numBoids];
+      boidBuffer.GetData(tempArray);
+      boids.CopyFrom(tempArray);
+    }
+
+    // GPU
+    if (val == 2)
+    {
+      mode = Modes.Gpu;
+    }
+  }
 
   void OnDestroy()
   {
+    if (boids != null)
+    {
+      boids.Dispose();
+    }
+    if (boids2 != null)
+    {
+      boids2.Dispose();
+    }
     if (boidBuffer != null)
     {
       boidBuffer.Release();
