89660f3dc50f2918135a4fbce0b0a336f05dd90c
Jason Tsorlinis <jtsorlinis@gmail.com>
2022-09-27 22:37:25 +1000

    Added movement in 3D

---
+++ Assets/MoveCamera3D.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera3D : MonoBehaviour
+{
+
+  [SerializeField] float moveSpeed = 10;
+  Camera cam;
+  Vector3 rotation;
+
+  float yRotationLimit = 88f;
+  float sensitivity = 3f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    cam = Camera.main;
+    rotation = new Vector3(0, -30, 0);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var vx = Input.GetAxis("Horizontal");
+    var vz = Input.GetAxis("Vertical");
+    var vy = Input.GetAxis("Jump");
+    var mouseX = Input.GetAxis("Mouse X");
+    var mouseY = Input.GetAxis("Mouse Y");
+    var mouseDown = Input.GetMouseButton(1);
+
+    if (mouseDown)
+    {
+      Cursor.lockState = CursorLockMode.Locked;
+      rotation.x += mouseX * sensitivity;
+      rotation.y += mouseY * sensitivity;
+      rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+      var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+      var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+      cam.transform.localRotation = xQuat * yQuat;
+
+      var movement = cam.transform.forward * vz * Time.deltaTime * moveSpeed;
+      movement += cam.transform.right * vx * Time.deltaTime * moveSpeed;
+      movement += cam.transform.up * vy * Time.deltaTime * moveSpeed;
+      cam.transform.position += movement;
+    }
+    else
+    {
+      Cursor.lockState = CursorLockMode.None;
+
+    }
+  }
+}
