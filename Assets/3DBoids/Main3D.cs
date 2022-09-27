638f18ce8be7bfd7f49a0f73eba464a9dc327b80
Jason Tsorlinis <jtsorlinis@gmail.com>
2022-09-27 18:14:58 +1000

    Increased maximum boids

--- Assets/3DBoids/Main3D.cs
+++ Assets/3DBoids/Main3D.cs
@@ -57,7 +57,7 @@ public class Main3D : MonoBehaviour
   Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 100);
 
   int cpuLimit = 2048;
-  int gpuLimit = 262144;
+  int gpuLimit = 524288;
 
   void Awake()
   {
